7d8f3f5ec54e8f8ab3dd6505ff09f8ecdd19e1fc
chintankhatri <chintankhatri7@hotmail.com>
2015-03-12 17:57:09 -0400

    post method complete

--- Documents/NetBeansProjects/Assignment4/src/java/services/Chintan.java
+++ Documents/NetBeansProjects/Assignment4/src/java/services/Chintan.java
@@ -18,7 +18,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-import org.json.simple.JSONObject;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
