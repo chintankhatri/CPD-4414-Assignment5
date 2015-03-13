645b6725b17e5d5e08f13f1b379362bbcacd6e8f
chintankhatri <chintankhatri7@hotmail.com>
2015-03-13 16:45:10 -0400

    start working for put method

--- Documents/NetBeansProjects/Assignment4/src/java/services/Chintan.java
+++ Documents/NetBeansProjects/Assignment4/src/java/services/Chintan.java
@@ -20,11 +20,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
